486fd9efaab33403748467d6fbf693f0b8ea2ad5
Othabteh <o.thabteh@gmail.com>
2020-11-21 22:45:05 +0200

    teat

--- osama-tset/linkedLists.js
+++ osama-tset/linkedLists.js
@@ -96,22 +96,82 @@ class LinkedList {
     }
     return currentNode.value;
   }
+  middle(value) {
+    const node = new Node(value);
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    let mid = Math.floor(count / 2) - 1;
+    let currentNode = this.head;
+    for (let i = 0; i < mid; i++) {
+      currentNode = currentNode.next;
+    }
+    node.next = currentNode.next;
+    currentNode.next = node;
+    return this.head;
+  }
 }
+
+function zipLists(LL1, LL2) {
+  let zipped = new LinkedList();
+  let current1 = LL1.head;
+  let current2 = LL2.head;
+  while (current1 || current2) {
+    if (current1) {
+      zipped.append(current1.value);
+      current1 = current1.next;
+    }
+    if (current2) {
+      zipped.append(current2.value);
+      current2 = current2.next;
+    }
+  }
+  return zipped;
+}
+
+function palindrome(LL) {
+  let arr = [];
+  let reversedArr = [];
+  let current = LL.head;
+  while (current) {
+    arr[arr.length] = current.value;
+    current = current.next;
+  }
+  console.log(arr);
+  for (let j = arr.length - 1; j >= 0; j--) {
+    reversedArr[reversedArr.length] = arr[j];
+  }
+  console.log(reversedArr);
+  let count = 0;
+  while (arr[count] == reversedArr[count]) {
+    count++;
+    return true;
+  }
+  return false;
+}
+
 let ll = new LinkedList();
+// let LL1 = new LinkedList();
+// let LL2 = new LinkedList();
 // ll.insert(5);
 // ll.insert(4);
 // ll.insert(3);
 // console.log(ll.toString());
-ll.append(5);
-ll.append(4);
-ll.append(3);
-ll.append(2);
 ll.append(1);
-ll.append(6);
-ll.append(7);
-ll.append(8);
-ll.append(9);
+ll.append(2);
+ll.append(3);
+ll.append(4);
+// ll.append(3);
+// ll.append(2);
+// ll.append(1);
+// console.log(ll);
+// const palindromee = palindrome(ll);
 
-console.log(ll.KthFromEnd(2));
+console.log(ll.middle(5));
+
+// console.log(zipper.toString());
 
 module.exports = LinkedList;
